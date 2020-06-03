3f870ba3707e8fe0bf56cc541e3992be9610db61
asantibanez <santibanez.andres@gmail.com>
2020-06-03 13:35:33 -0500

    Added ids for diffing

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -25,41 +25,41 @@
 
                 @if(!empty($value))
                     <div
+                        id="selected-value"
                         class="p-2 rounded border w-full bg-white"
                         wire:click.stop="selectValue(null)">
                         {{ $selectedOption['title'] }}
                     </div>
                 @else
-                    <form wire:submit.prevent="">
-                        <div class="relative">
-                            <input
-                                id="{{ $componentId }}-search"
-                                type="text"
-                                wire:model.debounce.300ms="searchTerm"
-                                placeholder="Search"
-                                class="p-2 rounded border w-full rounded"
-                            />
+                    <div id="options"
+                         class="relative">
+                        <input
+                            id="{{ $componentId }}-search"
+                            type="text"
+                            wire:model.debounce.300ms="searchTerm"
+                            placeholder="Search"
+                            class="p-2 rounded border w-full rounded"
+                        />
 
-                            <div class="absolute top-0 left-0 mt-12 w-full">
-                                @if($options->isNotEmpty())
-                                    <ul class="w-full bg-white border">
-                                        @foreach($options as $option)
-                                            <li
-                                                class="p-4 border-b hover:bg-indigo-100 cursor-pointer"
-                                                wire:click.stop="selectValue('{{ $option['value'] }}')">
-                                                {{ $option['description'] }}
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @endif
-                                @if(!empty($searchTerm) && $options->isEmpty())
-                                    <p class="p-8 w-full bg-white border text-center text-sm">
-                                        No options found
-                                    </p>
-                                @endif
-                            </div>
+                        <div class="absolute top-0 left-0 mt-12 w-full">
+                            @if($options->isNotEmpty())
+                                <ul class="w-full bg-white border">
+                                    @foreach($options as $option)
+                                        <li
+                                            class="p-4 border-b hover:bg-indigo-100 cursor-pointer"
+                                            wire:click.stop="selectValue('{{ $option['value'] }}')">
+                                            {{ $option['description'] }}
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                            @if(!empty($searchTerm) && $options->isEmpty())
+                                <p class="p-8 w-full bg-white border text-center text-sm">
+                                    No options found
+                                </p>
+                            @endif
                         </div>
-                    </form>
+                    </div>
                 @endif
 
             </div>
