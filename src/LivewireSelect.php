7bc6c6d20b226de6a4c056df35fe6dca6205eeea
asantibanez <santibanez.andres@gmail.com>
2020-06-10 21:53:06 -0500

    Added z-index to avoid inputs clashing

--- src/LivewireSelect.php
+++ src/LivewireSelect.php
@@ -206,7 +206,7 @@ class LivewireSelect extends Component
 
             'search' => 'relative',
             'searchInput' => 'p-2 rounded border w-full rounded',
-            'searchOptionsContainer' => 'absolute top-0 left-0 mt-12 w-full',
+            'searchOptionsContainer' => 'absolute top-0 left-0 mt-12 w-full z-10',
             'searchOptionItem' => 'bg-white p-3 border-b hover:bg-gray-100 cursor-pointer text-gray-700 text-sm',
             'searchNoResults' => 'p-8 w-full bg-white border text-center text-xs text-gray-600',
         ];
